dcaa57ce2d18733d589891205198ae7a9341e912
Marcelo <marceloo.dev@gmail.com>
2024-09-07 13:41:40 -0300

    alterações

--- scripts-condições/questao02.js
+++ scripts-condições/questao02.js
@@ -1,12 +1,11 @@
-function indentificao() {
 
-let = nome = prompt("Digite seu nome:");
+
+let = nome = prompt("Digite seu nome:"); 
 let = sexo = prompt("Digite F para Feminino ou M para Masculino:");
 
 let = estadoCivil = prompt("Digite o estdo civil:");
 
 if (sexo === "F" && estadoCivil === "casada") {
   let = tempoCasada = prompt("Quanto tempo de casada?");
-  alert(sexo, estadoCivil, tempoCasada);
 }
-}
+console.log(`${nome} é do Sexo ${sexo} e tem ${tempoCasada} de casada `);
