eca6876e0787c79549ffd545359ed21fcba4444d
Marcelo Carvalho <68958048+marcelocarvalho1@users.noreply.github.com>
2024-09-11 19:48:29 -0300

    Update questao04.js

--- scripts-condições/questao04.js
+++ scripts-condições/questao04.js
@@ -3,9 +3,9 @@ let B = parseInt(prompt("Digite o segundo valor:"));
 let C = 0;
 
 if (A === B){
-    let = C = (A + B);
+    C = (A + B);
 } else {
-    let = C = (A * B) ;
+    C = (A * B) ;
 }
 console.log(`o resultado é: ${C}`);
 
